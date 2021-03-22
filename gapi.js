65b71c304835e6e8fd2fbf52796674c2163a726e
Ajay Soliya <soliyaajay77@gmail.com>
2021-03-23 00:48:55 +0530

    added data.js

--- gapi.js
+++ gapi.js
@@ -1,13 +1,13 @@
 function ShareToFacebook(dataAndEvents,deepDataAndEvents){}
 function ShareToWebsites(id,ignoreMethodDoesntExist,dataAndEvents,deepDataAndEvents){}
 function ShareToTwitter(dataAndEvents,deepDataAndEvents){}
-function CreateLinksInGame(link_id,tp,key){var url="https://ganpats.github.io/NetworkingDemo";window.open(url);}
+function CreateLinksInGame(link_id,tp,key){var url="https://ganpats.github.io/NetworkingDemo/";window.open(url);}
 function OnGameStart(dataAndEvents,reply){console.log("call OnGameStart, nameid: "+dataAndEvents+", Times: "+reply);}
 function OnGamePause(dataAndEvents,reply){console.log("call OnGamePause, nameid: "+dataAndEvents+", Times: "+reply);}
 function OnGameLevelWin(reply,dataAndEvents){console.log("call OnGameLevelWin, nameid: "+reply);}
 function OnGameLevelFail(reply){console.log("call OnGameLevelFail, nameid: "+reply);}
 function GetLanguageInGame(label){return GamesLanguage.en[label];}
-var d=new String(window.location.host);if(d.indexOf("ganpats.github.io")==-1&&d.indexOf("localhost")==-1){window.location="https://ganpats.github.io/NetworkingDemo";}
+var d=new String(window.location.host);if(d.indexOf("ganpats.github.io/NetworkingDemo/")==-1&&d.indexOf("localhost")==-1){window.location="https://ganpats.github.io/NetworkingDemo/";}
 function CreateToolTipDiv(opt_attributes,expectedNumberOfNonCommentArgs,dataAndEvents){}
 function submitToFacebook(opt_id,deepDataAndEvents,dataAndEvents,until,onfail){}
 function FBOperation(opt_id,deepDataAndEvents,until,errorCB){}
