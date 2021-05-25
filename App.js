4ccff7a8c7b117df73e43d5b8f65dcf7860d2f33
Ganpatbhai Lakhara <ganpatlakhara@gmail.com>
2021-05-25 14:51:43 +0530

    update react-native npm

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, NavigatorIOS } from 'react-native';
 import SearchComponent from './src/components/SearchComponent';
-// Test change
+
 export default class App extends React.Component {
   render() {
     return (
